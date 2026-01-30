ca5cfb8d6f5ada6f9f236d0f60b778eba0b6ce4a
subeenhong <subeenyy@gmail.com>
2026-01-30 22:32:33 +0900

    chore: add logging to health check endpoint

--- src/main/java/org/example/common/controller/HealthController.java
+++ src/main/java/org/example/common/controller/HealthController.java
@@ -1,14 +1,18 @@
 package org.example.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthController {
 
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
+    public ResponseEntity<String> ping(@RequestHeader(value = "Host", required = false) String host) {
+        log.info("Health check request received. Host: {}", host);
         return ResponseEntity.ok("pong");
     }
 }
